09a7474048937d9c45948f7a4f558a8e152f49c6
Aki <mertunctuncer@gmail.com>
2024-06-23 14:08:04 +0300

    Initial commit

---
+++ app/src/main/java/com/github/mertunctuncer/ardunioserialmessager/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.github.mertunctuncer.ardunioserialmessager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
